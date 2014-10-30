a0f50bb9af9eabd7a044f56aff80737b1d5e3cfd
fatedier <512654112@qq.com>
2014-10-30 23:15:22 +0800

    Update setup.sh 2014-10-30

--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/env sh
 
-function fun ()
+function show_help()
 {
-    echo $#
-    echo $0
-    echo $1
-    echo $2
-    echo $3
-    return 3
+    echo -e "\nThe following contents will to be done:\n"
+    echo -e "1 Install Vundle(vim plugin) to ~/.vim/bundle/vundle/\n"
+    echo -e "2 Copy configuration files to corresponding directory\n"
+    echo -e "  including .aliascfg .gitconfig .vimrc .astylerc\n"
 }
 
-fun aa bb cc dd ee ff
-echo $?
+show_help
+echo -n "Are you sure to start? (Y/N)"
+read m_start_flag
+
+case ${m_start_flag} in
+y|Y)
+    echo you choose y
+    ;;
+n|N)
+    echo you choose n
+    ;;
+*)
+    echo you write wrong
+    ;;
+esac
