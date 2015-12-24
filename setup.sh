50f48a754edf7c8a38ecf2f21cde8c0b908ee2dd
fatedier <512654112@qq.com>
2015-12-24 23:18:10 +0800

    give me some shell color

--- setup.sh
+++ setup.sh
@@ -4,25 +4,31 @@
 use_shell='zsh'     # bash or zsh
 now_path=`pwd`
 
+# var
+lc='\033['
+rc='\033[0m'
+cred='31m'        # red
+cgreen='32m'        # green
+
 function show_help()
 {
-    echo -e "\nThe following contents will to be done:\n"
+    echo -e "\n${lc}${cgreen}The following contents will to be done:${rc}\n"
     echo -e "1 Install Vundle(vim plugin) to ~/.vim/bundle/vundle/\n"
     echo -e "2 Copy config files to corresponding directory\n"
-    echo -e "  including .aliascfg .gitconfig .vimrc\n"
+    echo -e "  including .aliascfg .gitconfig .vimrc .zshrc .tmux.conf\n"
     echo -e "3 Download some packages using yum\n"
 }
 
 function check_dir_vundle
 {
     if [ -e "${HOME}/.vim/bundle/vundle/" ]; then
-        echo "Vundle was already installed,setup will continue..."
+        echo -e "${lc}${cgreen}Vundle was already installed,setup will continue...${rc}"
     else
         echo -e "\nVundle is installing..."
         git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
         echo "Start to install plugin by vundle..."
         vim +BundleInstall +qa
-        echo "Install vundle success"
+        echo -e "${lc}${cgreen}Install vundle success${rc}"
     fi
 }
 
@@ -68,7 +74,7 @@ function copy_cfg_files
         cp -f .tmux.conf ~/
         echo "cp -f .tmux.conf ~/"
     fi
-    echo "Copy config files down"
+    echo -e "${lc}${cgreen}Copy config files down${rc}"
 }
 
 function add_include_to_bash_profile
@@ -77,7 +83,7 @@ function add_include_to_bash_profile
     grep "~/.aliascfg" ~/.bash_profile &>/dev/null
     if [ $? != 0 ]; then
         echo -e "\n# Include files\nif [ -f ~/.aliascfg ]; then\n    . ~/.aliascfg\nfi" >> ~/.bash_profile
-        echo "File .bash_profile doesn't include .aliascfg, add it success"
+        echo -e "${lc}${cgreen}File .bash_profile doesn't include .aliascfg, add it success${rc}"
     else
         echo ".aliascfg is already included"
     fi
@@ -94,10 +100,10 @@ function install_package
         echo "epel-release is not found, to install..."
         sudo yum install -y epel-release
         if [ $? -ne 0 ]; then
-            echo "Install epel-release failed"
+            echo -e "${lc}${cred}Install epel-release failed${rc}"
             exit -1
         fi
-        echo "Install epel-release success"
+        echo -e "${lc}${cgreen}Install epel-release success${rc}"
         sudo yum makecache
     fi
     # git
@@ -107,10 +113,10 @@ function install_package
         echo "git is not found, to install..."
         sudo yum install -y git
         if [ $? -ne 0 ]; then
-            echo "Install git failed"
+            echo -e "${lc}${cred}Install git failed${rc}"
             exit -1
         fi
-        echo "Install git success"
+        echo -e "${lc}${cgreen}Install git success"
     fi
     # vim
     echo "check vim..."
@@ -119,10 +125,10 @@ function install_package
         echo "vim is not found, to install..."
         sudo yum install -y vim
         if [ $? -ne 0 ]; then
-            echo "Install vim failed"
+            echo -e "${lc}${cred}Install vim failed${rc}"
             exit -1
         fi
-        echo "Install vim success"
+        echo -e "${lc}${cgreen}Install vim success${rc}"
     fi
     # gcc-c++
     echo "check gcc-c++..."
@@ -131,10 +137,10 @@ function install_package
         echo "g++ is not found, to install..."
         sudo yum install -y gcc-c++
         if [ $? -ne 0 ]; then
-            echo "Install gcc-c++ failed"
+            echo -e "${lc}${cred}Install gcc-c++ failed${rc}"
             exit -1
         fi
-        echo "Install gcc-c++ success"
+        echo -e "${lc}${cgreen}Install gcc-c++ success${rc}"
     fi
     # golang
     echo "check golang..."
@@ -143,10 +149,10 @@ function install_package
         echo "golang is not found, to install..."
         sudo yum install -y golang
         if [ $? -ne 0 ]; then
-            echo "Install golang failed"
+            echo -e "${lc}${cred}Install golang failed${rc}"
             exit -1
         fi
-        echo "Install golang success"
+        echo -e "${lc}${cgreen}Install golang success${rc}"
     fi 
     # ctags
     echo "check ctags..."
@@ -155,10 +161,10 @@ function install_package
         echo "ctags is not found, to install..."
         sudo yum install -y ctags 
         if [ $? -ne 0 ]; then
-            echo "Install ctags failed"
+            echo -e "${lc}${cred}Install ctags failed${rc}"
             exit -1
         fi
-        echo "Install ctags success"
+        echo -e "${lc}${cgreen}Install ctags success${rc}"
     fi
     # zsh
     echo "check zsh..."
@@ -167,10 +173,10 @@ function install_package
         echo "zsh is not found, to install..."
         sudo yum install -y zsh
         if [ $? -ne 0 ]; then
-            echo "Install zsh failed"
+            echo -e "${lc}${cred}Install zsh failed${rc}"
             exit -1
         fi
-        echo "Install zsh success"
+        echo -e "${lc}${cgreen}Install zsh success${rc}"
     fi
     # autojump-zsh
     echo "check autojump-zsh..."
@@ -179,10 +185,10 @@ function install_package
         echo "autojump-zsh is not found, to install..."
         sudo yum install -y autojump-zsh
         if [ $? -ne 0 ]; then
-            echo "Install autojump-zsh failed"
+            echo -e "${lc}${cred}Install autojump-zsh failed${rc}"
             exit -1
         fi
-        echo "Install autojump-zsh success"
+        echo -e "${lc}${cgreen}Install autojump-zsh success${rc}"
     fi
     # tmux
     echo "check tmux..."
@@ -191,10 +197,10 @@ function install_package
         echo "tmux is not found, to install..."
         sudo yum install -y zsh
         if [ $? -ne 0 ]; then
-            echo "Install tmux failed"
+            echo -e "${lc}${cred}Install tmux failed${rc}"
             exit -1
         fi
-        echo "Install tmux success"
+        echo -e "${lc}${cgreen}Install tmux success${rc}"
     fi
 }
 
@@ -202,13 +208,21 @@ function create_dir()
 {
     # git_fatedier
     if [ ! -d "${HOME}/local/git_fatedier" ]; then
-        echo "mkdir -p ${HOME}/local/git_fatedier"
         mkdir -p ${HOME}/local/git_fatedier
+        if [ $? -ne 0 ]; then
+            echo -e "${lc}${cred}mkdir -p ${HOME}/local/git_fatedier failed${rc}"
+        else
+            echo -e "${lc}${cgreen}mkdir -p ${HOME}/local/git_fatedier success${rc}"
+        fi
     fi
     # golang directory
     if [ ! -d "${HOME}/go_projects/src" ]; then
-        echo "mkdir -p ${HOME}/go_projects/src"
         mkdir -p ${HOME}/go_projects/src
+        if [ $? -ne 0 ]; then
+            echo -e "${lc}${cred}mkdir -p ${HOME}/go_projects/src failed${rc}"
+        else
+            echo -e "${lc}${cgreen}mkdir -p ${HOME}/go_projects/src success${rc}"
+        fi
     fi
 }
 
@@ -219,6 +233,11 @@ function download()
     if [ ! -d "${HOME}/local/git_fatedier/fatedier-tools" ]; then
         echo 'start download fatedier-tools...'
         git clone https://github.com/fatedier/fatedier-tools.git
+        if [ $? -ne 0 ]; then
+            echo -e "${lc}${cred}Download fatedier-tools failed${rc}"
+        else
+            echo -e "${lc}${cgreen}Download fatedier-tools success${rc}"
+        fi
         # compile some tools
         cd ${HOME}/local/git_fatedier/fatedier-tools/astyle && make
     fi
@@ -227,11 +246,17 @@ function download()
     if [ ! -d "${HOME}/local/git_fatedier/dot_file" ]; then
         echo 'start download dot_file...'
         git clone https://github.com/fatedier/dot_file.git
+        if [ $? -ne 0 ]; then
+            echo -e "${lc}${cred}Download dot_file failed${rc}"
+        else
+            echo -e "${lc}${cgreen}Download dot_file success${rc}"
+        fi
     fi
     # oh-my-zsh
     if [ ! -d "${HOME}/.oh-my-zsh" ]; then
         echo 'start download oh-my-zsh...'
         wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+        echo -e "${lc}${cgreen}Download oh-my-zsh over${rc}"
     fi
     cd ${now_path}
 }
@@ -242,11 +267,11 @@ function use_zsh()
     # 这里下载oh-my-zsh的脚本里已经执行过了
     # zsh
     # source ~/.zshrc
-    echo "You need to relogin to use zsh"
+    echo -e "${lc}${cgreen}You need to relogin to use zsh${rc}"
 }
 
 show_help
-echo -n "Are you sure to start? Make sure you have the sudo permissions (Y/N)"
+echo -ne "Are you sure to start? Make sure you have the ${lc}${cred}sudo${rc} permissions (Y/N)"
 read m_start_flag
 
 case ${m_start_flag} in
@@ -272,6 +297,6 @@ n|N)
     exit 0
     ;;
 *)
-    echo -e "#### error: your input is incorrect\n"
+    echo -e "${lc}${cred}#### error: your input is incorrect${rc}\n"
     ;;
 esac
