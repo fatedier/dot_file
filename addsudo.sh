f678d7b15f50507f9c36043b6a561d521fd406c7
fatedier <512654112@qq.com>
2015-12-25 11:17:00 +0800

    add addsudo.sh

---
+++ addsudo.sh
@@ -0,0 +1,26 @@
+#!/bin/env sh
+
+# config
+username='wcl'
+
+# var
+lc='\033['
+rc='\033[0m'
+cred='31m'
+cgreen='32m'
+
+now_user=`whoami`
+if [ "${now_user}X" != "root"X ]; then
+    echo -e "${lc}${cred}you should be the root user${rc}"
+    exit -1
+fi
+
+chmod o+w /etc/sudoers
+cat /etc/sudoers | grep ${username}$'\t'ALL=\(ALL\) &>/dev/null
+if [ $? != 0 ]; then
+    echo -e "${username}\tALL=(ALL)\tNOPASSWD: ALL" >> /etc/sudoers
+    echo -e "add sudo permission for user ${lc}${cred}${username}${rc}"
+else
+    echo -e "${lc}${cgreen}user ${username} already exist${rc}"
+fi
+chmod o-w /etc/sudoers
